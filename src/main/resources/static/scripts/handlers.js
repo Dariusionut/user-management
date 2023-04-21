d54ff090cd9552a62e38a7a3adc2ddc16c2bdb77
Darius <dariustinculescu@gmail.com>
2023-04-21 08:24:14 +0300

    Added bootstrap webjar and created confirm dialog for removing users

--- src/main/resources/static/scripts/handlers.js
+++ src/main/resources/static/scripts/handlers.js
@@ -1,5 +1,44 @@
 "use strict";
-const deleteUserHandler = function (formElement) {
-    const fullName = formElement.getAttribute('data-fullName');
-    return confirm(`Are you sure to permanently remove ${fullName}?`);
+const deleteUserHandler = function (element) {
+    const fullName = element.getAttribute('data-fullName');
+    const confirmModal = new ConfirmModal('#confirm-operation-modal');
+    confirmModal.content = `Are you sure to permanently remove ${fullName}?`;
+
+    const confirmHandler = function () {
+        const form = element.closest('form');
+        form.submit();
+    }
+    confirmModal.setConfirmHandler(confirmHandler);
+    confirmModal.show();
+}
+
+
+class ConfirmModal {
+
+    modal;
+    contentElement;
+    confirmBtn;
+    cancelBtn;
+
+    constructor(selector) {
+        const modalElement = document.querySelector(selector);
+        this.modal = new bootstrap.Modal(modalElement);
+        this.contentElement = document.getElementById('confirm-modal-message');
+        this.confirmBtn = document.getElementById('confirm-btn');
+        this.cancelBtn = document.getElementById('cancel-btn');
+    }
+
+
+    set content(contentString) {
+        this.contentElement.innerText = contentString;
+    }
+
+    show() {
+        this.modal.show();
+    }
+
+    setConfirmHandler(handler, capture = false) {
+        console.log(this.confirmBtn)
+        this.confirmBtn.addEventListener('click', handler, {capture: capture})
+    }
 }
