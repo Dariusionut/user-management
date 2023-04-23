2c1fb4ddfb94e94dca9ab2e70d5a5938dff4c941
DanAndrei <dargesanu@gmail.com>
2023-04-23 22:34:08 +0300

    I added fix-address-relation!

--- src/main/resources/db/03_table_address.sql
+++ src/main/resources/db/03_table_address.sql
@@ -1,22 +1,28 @@
 CREATE SEQUENCE IF NOT EXISTS seq_address;
 CREATE TABLE IF NOT EXISTS address
 (
-    address_id BIGINT UNIQUE NOT NULL DEFAULT nextval('seq_address'),
-    street     VARCHAR(100)  NULL,
-    city       VARCHAR(100)  NOT NULL,
-    state      VARCHAR(100)  NOT NULL,
-    zip        VARCHAR(20)   NOT NULL,
-
-    CONSTRAINT address_pk PRIMARY KEY (address_id)
+    id                 BIGINT      NOT NULL DEFAULT NEXTVAL('seq_address'),
+    country            VARCHAR(45) NOT NULL,
+    city               VARCHAR(45) NOT NULL,
+    door_number        VARCHAR(10) NOT NULL,
+    additional_details VARCHAR(65) NULL,
+    CONSTRAINT address_pk PRIMARY KEY (id)
 );
-INSERT INTO address (street, city, state, zip)
-VALUES ('Strada Libertatii', 'Bucuresti', 'Bucuresti', '010663'),
-       ('Bulevardul Unirii', 'Cluj-Napoca', 'Cluj', '400010'),
-       ('Bulevardul Corneliu Coposu', 'Timisoara', 'Timis', '300283'),
-       ('Strada General Traian Mosoiu', 'Oradea', 'Bihor', '410181'),
-       ('Bulevardul Regina Maria', 'Iasi', 'Iasi', '700506'),
-       ('Strada Vasile Lascar', 'Botosani', 'Botosani', '710233'),
-       ('Strada Horea', 'Alba Iulia', 'Alba', '510011'),
-       ('Bulevardul Eroilor de la Tisa', 'Satu Mare', 'Satu Mare', '440021'),
-       ('Strada Alexandru Vaida-Voevod', 'Sibiu', 'Sibiu', '550182'),
-       ('Strada Traian Vuia', 'Arad', 'Arad', '310130');
+INSERT INTO address (country, city, door_number, additional_details)
+VALUES ('USA', 'New York City', '1234', 'Apartment 5B');
+INSERT INTO address (country, city, door_number, additional_details)
+VALUES ('UK', 'London', '21', '3rd Floor');
+INSERT INTO address (country, city, door_number, additional_details)
+VALUES ('Australia', 'Sydney', '22', 'Beachfront');
+INSERT INTO address (country, city, door_number, additional_details)
+VALUES ('USA', 'Colorado', '54', 'Mountain View');
+INSERT INTO address (country, city, door_number, additional_details)
+VALUES ('UAE', 'Dubai', '1001', 'Luxury Living');
+INSERT INTO address (country, city, door_number, additional_details)
+VALUES ('Italy', 'Tuscany', '13', 'Vineyard View');
+INSERT INTO address (country, city, door_number, additional_details)
+VALUES ('Netherlands', 'Amsterdam', '33', 'Canal View');
+INSERT INTO address (country, city, door_number, additional_details)
+VALUES ('USA', 'Miami', '456', 'Oceanfront');
+INSERT INTO address (country, city, door_number, additional_details)
+VALUES ('France', 'French Alps', '7', 'Ski In/Out');
--- src/main/resources/db/04_table_app_user.sql
+++ src/main/resources/db/04_table_app_user.sql
@@ -14,27 +14,32 @@ CREATE TABLE IF NOT EXISTS app_user
 
     CONSTRAINT app_user_pk PRIMARY KEY (user_id),
     CONSTRAINT app_user_fk_role FOREIGN KEY (fk_role) REFERENCES role (role_id),
-    CONSTRAINT app_user_fk_address FOREIGN KEY (fk_address) REFERENCES address (address_id)
+    CONSTRAINT app_user_fk_address FOREIGN KEY (fk_address) REFERENCES address (id)
 );
 
-INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email)
-VALUES (1, NULL, 'Mary', 'Chen', 'marychen', 'password8', 'marychen@example.com');
-INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email)
-VALUES (2, NULL, 'John', 'Doe', 'johndoe', 'password9', 'johndoe@example.com');
-INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email)
-VALUES (3, 5, 'Jane', 'Doe', 'janedoe', 'password10', 'janedoe@example.com');
-INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email)
-VALUES (2, NULL, 'Alex', 'Lee', 'alexlee', 'password11', 'alexlee@example.com');
-INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email)
-VALUES (1, 7, 'Bob', 'Johnson', 'bobjohnson', 'password12', 'bobjohnson@example.com');
-INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email)
-VALUES (3, NULL, 'Emily', 'Wang', 'emilywang', 'password13', 'emilywang@example.com');
-INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email)
-VALUES (1, 3, 'David', 'Kim', 'davidkim', 'password14', 'davidkim@example.com');
-INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email)
-VALUES (2, NULL, 'Chris', 'Johnson', 'chrisjohnson', 'password15', 'chrisjohnson@example.com');
-INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email)
-VALUES (3, 9, 'Anna', 'Lee', 'annalee', 'password16', 'annalee@example.com');
-INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email)
-VALUES (1, NULL, 'Tom', 'Brown', 'tombrown', 'password17', 'tombrown@example.com');
+INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email, is_enabled, date_added)
+VALUES (3, 1, 'Alice', 'Smith', 'alicesmith', 'password789', 'alice.smith@example.com', FALSE, NOW());
+
+INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email, is_enabled, date_added)
+VALUES (1, 2, 'Bob', 'Johnson', 'bobjohnson', 'passwordabc', 'bob.johnson@example.com', TRUE, NOW());
+
+INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email, is_enabled, date_added)
+VALUES (2, NULL, 'Sarah', 'Lee', 'sarahlee', 'passworddef', 'sarah.lee@example.com', TRUE, NOW());
+
+INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email, is_enabled, date_added)
+VALUES (3, 3, 'Mike', 'Brown', 'mikebrown', 'passwordxyz', 'mike.brown@example.com', TRUE, NOW());
+
+INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email, is_enabled, date_added)
+VALUES (2, 4, 'Emily', 'Wong', 'emilywong', 'password123', 'emily.wong@example.com', FALSE, NOW());
+
+INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email, is_enabled, date_added)
+VALUES (1, NULL, 'David', 'Kim', 'davidkim', 'password456', 'david.kim@example.com', TRUE, NOW());
+
+INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email, is_enabled, date_added)
+VALUES (3, NULL, 'Julia', 'Chen', 'juliachen', 'password789', 'julia.chen@example.com', TRUE, NOW());
+
+INSERT INTO app_user (fk_role, fk_address, first_name, last_name, username, password, email, is_enabled, date_added)
+VALUES (1, 5, 'Ryan', 'Garcia', 'ryangarcia', 'passwordabc', 'ryan.garcia@example.com', FALSE, NOW());
+
+
 
