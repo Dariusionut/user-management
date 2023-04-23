75257a0096734daee2c871fd5ce7a72b937bd729
Darius <dariustinculescu@gmail.com>
2023-04-23 17:20:31 +0300

    Updated home endpoint

--- src/main/java/com/spring/studentmanagement/controllers/HomeController.java
+++ src/main/java/com/spring/studentmanagement/controllers/HomeController.java
@@ -2,17 +2,15 @@ package com.spring.studentmanagement.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created at 4/19/2023 by Darius
  **/
 
 @Controller
-@RequestMapping(path = "/home")
 public class HomeController {
 
-    @GetMapping
+    @GetMapping(path = "/home")
     public String getHomeView() {
 
         return "index";
