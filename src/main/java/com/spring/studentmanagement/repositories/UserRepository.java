4370586723d3333dabc2a76d2cd4c6c5b19f4b6a
Stefi <gonciarstef@yahoo.com>
2023-04-25 00:18:04 +0300

    46 - find by username or email

--- src/main/java/com/spring/studentmanagement/repositories/UserRepository.java
+++ src/main/java/com/spring/studentmanagement/repositories/UserRepository.java
@@ -2,7 +2,11 @@ package com.spring.studentmanagement.repositories;
 
 import com.spring.studentmanagement.models.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * Created at 4/19/2023 by Darius
@@ -10,4 +14,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
+
+    @Query(value="SELECT u FROM AppUser u WHERE u.username  = ?1 OR u.email = ?1")
+    Optional<AppUser> findBYUsernameOrEmail(@Param("usernameOrEmail") String usernameOrEmail);
 }
