09430cdbe9a74424863e600b52d95641d5293455
DanAndrei <dargesanu@gmail.com>
2023-04-24 21:18:02 +0300

    I added the saveUser method in UserSevice!

--- src/main/java/com/spring/studentmanagement/services/UserService.java
+++ src/main/java/com/spring/studentmanagement/services/UserService.java
@@ -2,16 +2,20 @@ package com.spring.studentmanagement.services;
 
 import com.spring.studentmanagement.exceptions.UserNotFoundExceptions;
 import com.spring.studentmanagement.models.AppUser;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 /**
  * Created at 20.04.2023 by Dan.
  */
+@Transactional
 public interface UserService {
     List<AppUser> findAllUsers();
 
     void deleteUserById(Long userId);
 
     AppUser getUserById(Long userId) throws UserNotFoundExceptions;
+
+    AppUser saveUser(AppUser user);
 }
