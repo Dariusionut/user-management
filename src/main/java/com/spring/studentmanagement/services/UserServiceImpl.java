2f1f48c195b9c447f95a8297f5fd2b62249b227f
DanAndrei <dargesanu@gmail.com>
2023-04-24 21:21:31 +0300

    I implemented the saveUser method in Sevice

--- src/main/java/com/spring/studentmanagement/services/UserServiceImpl.java
+++ src/main/java/com/spring/studentmanagement/services/UserServiceImpl.java
@@ -3,8 +3,8 @@ package com.spring.studentmanagement.services;
 import com.spring.studentmanagement.exceptions.UserNotFoundExceptions;
 import com.spring.studentmanagement.models.AppUser;
 import com.spring.studentmanagement.repositories.UserRepository;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,11 +13,14 @@ import java.util.List;
  * Created at 20.04.2023 by Dan.
  */
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public List<AppUser> findAllUsers() {
@@ -34,7 +37,13 @@ public class UserServiceImpl implements UserService {
         log.info("Trying to getUserById with param: userId = {}", userId);
 
         return this.userRepository.findById(userId).orElseThrow(() -> new UserNotFoundExceptions("User not found!"));
+    }
 
+    @Override
+    public AppUser saveUser(AppUser user) {
+        String hashedPassword = new BCryptPasswordEncoder().encode(user.getPassword());
+        user.setPassword(hashedPassword);
+        return userRepository.save(user);
     }
 }
 
