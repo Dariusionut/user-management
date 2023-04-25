e70f4c9b466e7ae272b9306d1f980ec527a97f11
Darius <dariustinculescu@gmail.com>
2023-04-25 07:59:21 +0300

    Make AuthenticationException a checked exception

--- src/main/java/com/spring/studentmanagement/security/AppAuthManager.java
+++ src/main/java/com/spring/studentmanagement/security/AppAuthManager.java
@@ -1,11 +1,12 @@
 package com.spring.studentmanagement.security;
 
 import com.spring.studentmanagement.controllers.requests.LoginRequest;
+import com.spring.studentmanagement.exceptions.AuthenticationException;
 import com.spring.studentmanagement.models.AppUser;
 
 /**
  * Created at 4/25/2023 by Darius
  **/
 public interface AppAuthManager {
-    AppUser authenticate(LoginRequest loginRequest);
+    AppUser authenticate(LoginRequest loginRequest) throws AuthenticationException;
 }
