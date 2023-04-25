b4731d9ed23051edfdec86cfba331640fb8a9625
Darius <dariustinculescu@gmail.com>
2023-04-26 00:58:28 +0300

    Added functionality to disable security

--- src/main/java/com/spring/studentmanagement/security/filters/AuthFilter.java
+++ src/main/java/com/spring/studentmanagement/security/filters/AuthFilter.java
@@ -6,8 +6,9 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,11 +19,9 @@ import java.io.IOException;
  **/
 
 @Component
+@ConditionalOnProperty(name = "security.enabled", havingValue = "true")
 @Slf4j
 public class AuthFilter extends OncePerRequestFilter {
-
-    @Value("${security.enabled}")
-    private boolean securityEnabled;
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
@@ -39,18 +38,15 @@ public class AuthFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (this.securityEnabled) {
-            final HttpSession session = request.getSession();
-            AppUser user = (AppUser) session.getAttribute("userPrincipal");
-            if (user == null) {
-                response.sendRedirect("/user-management/errors/error-401");
-            }
-            log.warn("user = {}", user);
-            log.warn("Filter  url = {}", request.getRequestURI());
-        } else {
-            log.warn("Security mode is disabled!");
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final HttpSession session = request.getSession();
+        AppUser user = (AppUser) session.getAttribute("userPrincipal");
+        if (user == null) {
+            response.sendRedirect("/user-management/errors/error-401");
         }
+        log.warn("user = {}", user);
+        log.warn("Filter  url = {}", request.getRequestURI());
+
         filterChain.doFilter(request, response);
     }
 }
