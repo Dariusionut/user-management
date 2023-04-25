a6e3b00adca65caa3c910da1ddbf3596d0626921
Stefi <gonciarstef@yahoo.com>
2023-04-25 13:13:01 +0300

    added PostMApping in UserController

--- src/main/java/com/spring/studentmanagement/controllers/UserController.java
+++ src/main/java/com/spring/studentmanagement/controllers/UserController.java
@@ -6,6 +6,7 @@ import com.spring.studentmanagement.services.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -49,6 +50,13 @@ public class UserController {
     public String deleteUser(@RequestParam("userId") Long userId) {
         this.userService.deleteUserById(userId);
         return "redirect:/users";
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public String create(@RequestBody AppUser user) {
+        this.userService.saveUser(user);
+        return "redirect:/login";
     }
 
 
