80fee1db344a93d138b24ce839c9e504e88508c4
Stefi <gonciarstef@yahoo.com>
2023-04-26 17:29:20 +0300

    Updated CourseRepository

--- src/main/java/com/spring/studentmanagement/repositories/CourseRepository.java
+++ src/main/java/com/spring/studentmanagement/repositories/CourseRepository.java
@@ -1,7 +1,6 @@
 package com.spring.studentmanagement.repositories;
 
 import com.spring.studentmanagement.models.Course;
-import com.spring.studentmanagement.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
