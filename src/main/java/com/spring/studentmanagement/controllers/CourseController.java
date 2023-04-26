9773d66251580c96e46cbcf2b524b2ce29c674eb
Stefi <gonciarstef@yahoo.com>
2023-04-26 16:33:18 +0300

    Added: -course category sql -course sql -CourseCategory.java -Course.java -starting CourseController.java

---
+++ src/main/java/com/spring/studentmanagement/controllers/CourseController.java
@@ -0,0 +1,13 @@
+package com.spring.studentmanagement.controllers;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/courses")
+@RequiredArgsConstructor
+@Slf4j
+public class CourseController {
+}
